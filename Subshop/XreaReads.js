838c3c87ccd1bf0076a68ed724c097fc7d97df03
Prince <153154117+Andeasw@users.noreply.github.com>
2025-12-06 19:33:12 +0800

    Update XreaReads.js

--- Subshop/XreaReads.js
+++ Subshop/XreaReads.js
@@ -1,18 +1,30 @@
-<script>
-  (function () {
-    const langs = (navigator.languages && navigator.languages.length)
-      ? navigator.languages
-      : [navigator.language || ""];
+<!--nobanner-->
+<script type="text/javascript">
+(function() {
+    // ▼▼▼▼
+    var adSrc = "https://cache1.value-domain.com/xa.j?site=xxxxx.s331.xrea.com";
+    // ▲▲▲▲
 
-    const isZh = langs.some(l => String(l).toLowerCase().startsWith("zh"));
+    var lang = (navigator.language || navigator.userLanguage || "en").toLowerCase();
 
-    if (isZh) {
-      document.documentElement.setAttribute("data-nobanner", "true");
+    if (lang.indexOf('zh') === -1) {
+        var containerId = "my-custom-ad-box";
+      
+        document.write('<style>');
+        document.write('#' + containerId + ' { position: fixed; bottom: 0; left: 0; z-index: 99; background: transparent; }');
+        document.write('</style>');
 
-      const banner = document.querySelector("#banner");
-      if (banner) banner.style.display = "none";
+        document.write('<div id="' + containerId + '">');
+        document.write('<script type="text/javascript" src="' + adSrc + '"><\/script>');
+        document.write('</div>');
 
-      document.documentElement.appendChild(document.createComment("nobanner"));
+        setTimeout(function() {
+            var box = document.getElementById(containerId);
+            if (box) {
+                box.style.display = 'none';
+                // box.remove();
+            }
+        }, 7000);
     }
-  })();
+})();
 </script>
